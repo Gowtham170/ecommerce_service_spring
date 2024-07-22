190408eb8792100e7aa5699d6c65dc356f16ae9b
gowtham170 <gowthamanbarasu@Gowthams-Laptop.local>
2024-07-22 17:39:48 +0530

    Initial commit

---
+++ src/main/java/com/example/ecommerce_service/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String sayHello() {
+        return  "Hello";
+    }
+}
